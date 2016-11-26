ca5e9f735979f397ca312de1392a9868c86f3065
daveScof <davescofab@gmail.com>
2016-11-27 01:08:40 +0300

    Makes QuizActivity call CheatActivity when Cheat button is pressed

--- app/src/main/java/com/qene/android/geoquiz/CheatActivity.java
+++ app/src/main/java/com/qene/android/geoquiz/CheatActivity.java
@@ -1,7 +1,10 @@
 package com.qene.android.geoquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CheatActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class CheatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
+
+
     }
 }
--- app/src/main/java/com/qene/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/qene/android/geoquiz/QuizActivity.java
@@ -1,10 +1,10 @@
 package com.qene.android.geoquiz;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -15,6 +15,7 @@ public class QuizActivity extends AppCompatActivity {
 
     private static final String TAG = "QuizActivity";
     private static final String KEY_INDEX = "index";
+
 
     private TextView mQuestionTextView;
 
@@ -91,12 +92,22 @@ public class QuizActivity extends AppCompatActivity {
                 checkAnswer(true);
             }
         });
+
         Button falseButton = (Button) findViewById(R.id.false_button);
         falseButton.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick (View view){
                checkAnswer(false);
+            }
+        });
+
+        Button cheatButton = (Button) findViewById(R.id.cheatButton);
+        cheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(QuizActivity.this, CheatActivity.class);
+                startActivity(i);
             }
         });
     }
